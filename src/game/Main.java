873aef0ea4c3663b1f8771770773281d3d3ca112
Dalton Hodkiewicz <dhodkiewicz@gmail.com>
2022-03-30 22:05:01 -0500

    Room construction
    Arbitrary way of populating room coordinates// starting and ending door locations for now
    
    Need to work on amt of rooms- logic for linking rooms together upon instantiation. If we populate (construct) all of the doors at once, this would create an issue with needing to create another dimension.. A good way to do that will be to have it so once a door is closed or a room is left- and that room is decomposed and an entirely new room is created. The logic behind can be tied together another way unless someone has another suggestion!

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Point;
+
 public class Main {
 
 	public Main() {
@@ -8,8 +10,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game();
-		Room room = new Room();
+		Room room = new Room(); // rooms will automatically be constructed in game constructor but this was just for testing purposes
 		System.out.println(room.getDimensions());
+		for(Point point : room.getRoomCoordinates()) {
+			System.out.println(point); // room coordinates populating and instantiating correctly :) Next need to define amount of Rooms upon instantiation and logic
+		}                             // behind linking them together
+		
+		room.setDoors();
+		System.out.println(room.getStartingDoor()); // arbitrary way of setting door locations for now- will work on logic later
+		System.out.println(room.getEndingDoor());
+		
 
 	}
 
--- src/game/Room.java
+++ src/game/Room.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,6 +10,7 @@ import entity.Npc;
 public class Room {
 	
 	private Point dimensions;
+	private List <Point> roomCoordinates;
 	private Point startingDoor;
 	private Point endingDoor;
 	private Point userLocation;
@@ -26,8 +28,11 @@ public class Room {
 		randomValue = (int)Math.floor(Math.random()*(upper_bound-lower_bound+1)+ lower_bound);
 		dimensions.y = randomValue;
 		
+		populateRoomCoordinates(); // populate room coordinates upon instantiation
+		
 	}
 	
+	/////////////////////////////////////////////////////////DOOR METHODS START//////////////////////////////////////////////////////////////////////////////
 	//get door
 	public Point getStartingDoor() {
 		return startingDoor;
@@ -75,5 +80,39 @@ public class Room {
 	public void setNpcs(List<Npc> npcs) {
 		this.npcs = npcs;
 	}
+	
+	//set the doors
+	public void setDoors() {
+		int y = this.dimensions.y / 2;
+		int x = 0; // for now pretend on x and y axis- that these doors will be connected from Left to Right order
+		Point door = new Point(x,y); // create starting point for room
+		this.startingDoor = door;
+		
+		y = this.dimensions.y / 2; // setting y coordinate to be half the rooms dimension on y axis- so doors will be in the middle of room y- axis wall
+		x = this.dimensions.x - 1; // this will set the x coordinate for the end door to the farthest right of the room (in theory)
+		door = new Point(x,y);
+		this.endingDoor = door; // now set ending door
+		
+		
+	}
+
+	// retrieve the rooms coordinates
+	public List <Point> getRoomCoordinates() {
+		return roomCoordinates;
+	}
+	
+	//method to populate room coordinates
+	public void populateRoomCoordinates() {
+		List<Point> tempCoordinates= new ArrayList<Point>();
+		for (int x = 0; x < this.dimensions.x; x++){
+			for(int y = 0; y< this.dimensions.y;y++) {
+				Point p = new Point(x,y);
+				tempCoordinates.add(p);
+			}
+		}
+		this.roomCoordinates = new ArrayList<Point>(); // create instance in memory of coordinates so roomcoordinates isn't null value
+		this.roomCoordinates = tempCoordinates; // instantiate property with temporary coordinates
+	}
+
 
 }
