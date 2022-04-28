e6ce6c8d2a431c65cf9bef3f8ecee48571909f0b
vgomez5 <90812262+vgomez5@users.noreply.github.com>
2022-04-28 15:51:02 -0500

    Revert "Updated Item and Backpack"
    This reverts commit 64badffbb9de6c3c54932ee156356c45fd488c37.

--- src/item/Item.java
+++ src/item/Item.java
@@ -5,11 +5,11 @@ package item;
 
 import java.awt.Point;
 
+
 public class Item {
 
-	private Point location;	// The location of the item.
-	private itemType type;  //private variable for holding itemtype
-	private String itemDescription; // Describes the item.
+	private Point location;
+	private itemType type; //private variable for holding itemtype
 
 	public enum itemType{healthPotion,longsword,shortsword,axe,knife,flashlight,key,map} // enum that holds our types
 
@@ -17,36 +17,9 @@ public class Item {
 
 	}
 
-	/**
-	 * Constructor - Items that will be given to user at the start of a game.
-	 * @param type
-	 * @param desc
-	 */
-	public Item(itemType type , String desc) {
-		// Set the item type and description.
-		setType(type);
-		setItemDescription(desc);
-	}
-	
-	
 	public Item(itemType type, Point location) {
 		this.type = type;
 		this.location = location;
-	}
-	
-	
-	/**
-	 * Constructor - Used for items that will be scattered in a room for the user to find.
-	 * 			1.3 - Use of overloaded method/constructor.
-	 * @param type
-	 * @param desc
-	 * @param itemLocation
-	 */
-	public Item(itemType type, String desc, Point itemLocation) {
-		// Set the item type, description, and location.
-		setType(type);
-		setItemDescription(desc);
-		setLocation(itemLocation);
 	}
 
 	//getter for itemtype
@@ -86,38 +59,6 @@ public class Item {
 	public void setItemDescription(String itemDesc)
 	{
 		this.itemDescription = itemDesc;
-	}
-	
-	//method to help easily set item types
-	public itemType setTypeByInt(int value) {
-		itemType type = null;
-			
-		if(value == 0) {
-			type = itemType.key;
-		}
-		if(value == 1) {
-			type = itemType.flashlight;
-		}
-		if(value == 2) {
-			type = itemType.map;
-		}
-		if(value == 3) {
-			type = itemType.healthPotion;
-		}
-		if(value == 4) {
-			type = itemType.longsword;
-		}
-		if(value == 5) {
-			type = itemType.shortsword;
-		}
-		if(value == 6) {
-			type = itemType.axe;
-		}
-		if(value == 7) {
-			type = itemType.knife;
-		}
-			
-		return type;
 	}
 
 
