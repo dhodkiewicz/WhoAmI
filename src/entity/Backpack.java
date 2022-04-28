64badffbb9de6c3c54932ee156356c45fd488c37
vgomez5 <90812262+vgomez5@users.noreply.github.com>
2022-04-28 14:22:22 -0500

    Updated Item and Backpack
    Added code that was lost due to conflicts when I couldn't access the Vivian branch.

--- src/entity/Backpack.java
+++ src/entity/Backpack.java
@@ -6,63 +6,55 @@ package entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import item.Item;
 
 public class Backpack {
 
-	public static List<Item>backpack = null;
+	// 1.5 - Use of the Static keyword
+	private List<Item> backpack = null; // List of items in the backpack.
+
+	// Constructor
+	private Backpack() {
+		// Create the backpack (a list of items).
+		backpack = new ArrayList<Item>();
+	}
+
+	/**
+	 * Creates the single instance of Backpack that will be shared with classes.
+	 */
+	private static final Backpack instance = new Backpack();
+
+	/**
+	 * Gets the instance of Backpack.
+	 * @return instance
+	 */
+	public static Backpack getInstance() {
+		return instance;
+	}
+
+	/**
+	 * This gets the list of items in the backpack.
+	 * @return backpack
+	 */
+	public synchronized List<Item> getBackpack() {
+		return this.backpack;
+	}
+
+	/**
+	 * Add items to the backpack.
+	 * @param item
+	 */
+	public synchronized void addToBackpack(Item item) {
+		this.backpack.add(item);
+	}
+
+	/**
+	 * 1.4a - Use of override of .toString() in at least one scenario.
+	 */
+	@Override
+	public String toString() {
+
+		return backpack.toString();
+	}
 }
-
-
-
-
-/*
- * IGNORE. Random code used for testing.
- *
-	Backpack backpack = Backpack.getInstance();
-
-	Item shortSword = new Item("shortsword", "I am a short sword");
-	Item axe = new Item("axe", "I am an axe");
-
-
-	backpack.addToBackpack(shortSword);
-	backpack.addToBackpack(axe);
-
-	if (backpack.getBackpack() != null) {
-		for(Item i : backpack.getBackpack()) {
-			System.out.println(i.getItemDescription());
-		}
-
-		//System.out.println(backpack.getBackpack());
-	}
-	else {
-		System.out.println("There are no items in the backpack.");
-	}
-
-	System.out.println(backpack.toString());
-
-	System.out.println("Type the name of an item to find in your backpack.");
-
-	String itemName = in.nextLine();
-
-	if (!itemName.isBlank())
-	{
-		Item itemFound = backpack.findItem().apply(itemName);
-		if(itemFound != null)
-		{
-			System.out.println("The item you were looking for is in the backpack. What would you like to do with it?");
-		}
-		else
-		{
-			System.out.println("The item you are looking for is not in the backpack. Try looking for a different item.");
-		}
-
-	}
-	else
-	{
-			System.out.println("Please enter an item name.");
-	}
-
- */
