320bd25e5dfa46d04082a2ba9abe29f07316bbcf
Dalton Hodkiewicz <dhodkiewicz@gmail.com>
2022-04-29 13:33:30 -0500

    Create HealthPotion.java
    created healthpotion

---
+++ src/item/HealthPotion.java
@@ -0,0 +1,16 @@
+package item;
+
+import java.awt.Point;
+
+public class HealthPotion extends Item {
+
+	public HealthPotion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HealthPotion(itemType type, Point location) {
+		super(type, location);
+		// TODO Auto-generated constructor stub
+	}
+
+}
