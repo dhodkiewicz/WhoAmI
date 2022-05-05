d5cac6cc2dd928e8ee0034d1425d2f43461dbe13
vgomez5 <90812262+vgomez5@users.noreply.github.com>
2022-05-05 15:43:20 -0500

    Npc / Battle updates
    Added Battle interface and implemented it in Npc class.

--- src/NPC/Knight.java
+++ src/NPC/Knight.java
@@ -9,6 +9,7 @@ import java.awt.Point;
 public class Knight extends Npc {
 
 	private final String name = "Knight";	// The name of the NPC.
+	private double health = 25;				// The knight's starting health.
 	
 	public Knight(int id) {
 		super(id);
@@ -18,21 +19,25 @@ public class Knight extends Npc {
 			this.setAttack(5);
 			this.setDescription("A knight stands here with a dagger.");
 			this.setMessage("Why do you linger? To get through this room you must get through me.");
+			this.setHealth(health);
 		}
 		if (id == 2) {
 			this.setAttack(7.5);
 			this.setDescription("A knight stands here with a sword.");
 			this.setMessage("I wouldn't if I were you.");
+			this.setHealth(health);
 		}
 		if (id == 3) {
 			this.setAttack(10);
 			this.setDescription("A knight stands here with a torch.");
 			this.setMessage("Are you looking for trouble? Because you're about to find some.");
+			this.setHealth(health);
 		}
 		if (id == 4) {
 			this.setAttack(12.5);
 			this.setDescription("A knight stands here with a flail.");
 			this.setMessage("You've come this far only to lose this battle.");
+			this.setHealth(health);
 		}
 	}
 
@@ -45,7 +50,14 @@ public class Knight extends Npc {
 		return name;
 	}
 	
-	// -------------------------
-	//    Create battle here?
-	// -------------------------
+//	// Gets the health of this type of NPC.
+//	public double getHealth() {
+//		return health;
+//	}
+//	
+//	// Sets the health of this type of NPC.
+//	public void setHealth(double health) {
+//		this.health = health;
+//	}
+
 }
